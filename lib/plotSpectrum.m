731fd5dce8ca192679441abe210b9de2d255ae58
Morgan Prior <morgan@amaras-tech.co.uk>
2014-02-12 08:54:08 +0000

    Allow plotspectrum to  accept more plot formatting commands a sa cell

--- lib/plotSpectrum.m
+++ lib/plotSpectrum.m
@@ -1,7 +1,15 @@
 function plotSpectrum(X,fin,fmt)
 % plotSpectrum(X,fin,fmt) Plot a smoothed spectrum
+% fmt a basic graph format '-', 'r' etc OR
+%      a cell of parameters to be passed to semilogx ie:
+%      {'color', [0.5 0.5 0.5]}
+% Calls semilogx
 if nargin<3
     fmt='-';
 end
 [f p] = logsmooth(X,fin);
-semilogx(f,p,fmt);
+if iscell(fmt)
+    semilogx(f,p,fmt{:});
+else
+    semilogx(f,p,fmt);
+end
